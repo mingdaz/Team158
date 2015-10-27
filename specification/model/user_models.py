015e265a4a86c6c3b87e2c32abeed6b9039f3aa4
jgong1 <michaelgongjian@gmail.com>
2015-10-26 21:23:22 -0400

    Remove is_teacher flag

--- specification/model/user_models.py
+++ specification/model/user_models.py
@@ -13,7 +13,6 @@ class Learner(models.Model):
     lesson_plan = models.IntegerField(default = 1)  # MAX = 5
     follows = models.ManyToManyField(user)
     photo = models.ImageField(upload_to = 'user_photo', blank = True)
-    is_teacher = models.BooleanField(default = False) # read only
 
     def __unicode__(self):
         return 'Learner' + self.user.username
@@ -22,8 +21,7 @@ class Learner(models.Model):
 class Teacher(models.Model):
     user = models.OneToOneField(User, unique = True)
     bio = models.CharField(max_length = 420)
-    photo = models.ImageField(upload_to = 'user_photo', blank = True)    
-    is_teacher = models.BooleanField(default = True) # read only
+    photo = models.ImageField(upload_to = 'user_photo', blank = True)
 
     def __unicode__(self):
         return 'Teacher' + self.user.username
