e46364bdc42bf96a6bfe180e86394120955424bc
jgong1 <michaelgongjian@gmail.com>
2015-10-26 23:55:24 -0400

    nothing changed

--- chingoal/basic/message_models.py
+++ chingoal/basic/message_models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 from basic.user_models import *
 
-class message(models.Model):
+class Message(models.Model):
     author = models.ForeignKey(User)
     text = models.CharField(max_length = 4200)
     post_time = models.DateTimeField(auto_now_add = True)
