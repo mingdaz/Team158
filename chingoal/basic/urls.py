9882c95121c7f532ce8733b0cf57452d6eb22d44
mingdaz <zhangmd406@gmail.com>
2015-10-28 18:38:46 -0400

    add url

--- chingoal/basic/urls.py
+++ chingoal/basic/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^register$', 'basic.user_manage.register', name = 'register'),
     url(r'^login$', 'django.contrib.auth.user_manage.login', {'template_name':'login.html'}),
     url(r'^logout$', 'django.contrib.auth.user_manage.logout_then_login', name = 'logout'),
-    url(r'^edit-profile$', 'basic.user_manage.edit_profile', name= 'editProfile', name = 'editProfile'),
+    url(r'^edit-profile$', 'basic.user_manage.edit_profile', name= 'editProfile'),
     url(r'^view-profile/(?P<uname>\w+)$', 'basic.user_manage.view_profile', name = 'viewProfile'),
     url(r'^reset-password$', 'basic.user_manage.reset_password', name = 'resetPassword'),
     url(r'^new-password/(?P<token>.*)$', 'basic.user_manage.new_password', name = 'newPassword'),
