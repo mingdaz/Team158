8e688773e34624703cd8fb5ba4ff1f1a6bd8b2e1
mingdaz <zhangmd406@gmail.com>
2015-10-28 21:32:00 -0400

    change setting and url

--- chingoal/chingoal/settings.py
+++ chingoal/chingoal/settings.py
@@ -15,6 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__)) + '/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -25,7 +26,30 @@ SECRET_KEY = '1iwo)ymjks)sxp(0esky1q61#)n)9fh-tt&kz=z45*w1mxd96^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+TEMPLATE_DEBUG = True
+
 ALLOWED_HOSTS = []
+
+TEMPLATE_DIRS = (
+    PROJECT_PATH + '/templates/',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    PROJECT_PATH + '/static/',
+)
 
 
 # Application definition
@@ -37,7 +61,13 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'basic'
+    
+    'home',
+    'account',
+    'testpage',
+    'store',
+    'discussion',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -52,6 +82,11 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'chingoal.urls'
+
+LOGIN_URL = '/account/login'
+
+# Default URL to redirect to after a user logs in.
+LOGIN_REDIRECT_URL = '/'
 
 TEMPLATES = [
     {
@@ -88,7 +123,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Eastern'
 
 USE_I18N = True
 
