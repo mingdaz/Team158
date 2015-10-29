1019674b112619e1d7d07e0515a135f1cde20bb8
mingdaz <zhangmd406@gmail.com>
2015-10-28 21:28:20 -0400

    add discussion

---
+++ chingoal/discussion/urls.py
@@ -0,0 +1,8 @@
+
+from django.conf.urls import include, url
+
+urlpatterns = [
+    url(r'^post-question$','discussion.views.post_question',name ='post' ),
+    url(r'^reply-question$','discussion.views.reply_question',name ='learningsavequestion' ),
+    url(r'^delete-post$','discussion.views.delete_post',name ='learningeditquestion' ),
+   ]
