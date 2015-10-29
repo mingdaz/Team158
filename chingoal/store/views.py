3a3209cfad400265b01b862e06ac70f11c0e8ff7
jingwen qiang <jxq48@jingwens-MBP.wv.cc.cmu.edu>
2015-10-29 13:16:13 -0400

    modify views

--- chingoal/store/views.py
+++ chingoal/store/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from models import *
 # Create your views here.
+@login_required
+def buy_title(request):
+    context = {}
+    return render(request,'store.html',context)
+
+@login_required
+def unlock_learning(request):
+    context = {}
+    return render(request,'store.html',context)
+
+@login_required
+def earn_money(request):
+    return HttpResponse('success')
