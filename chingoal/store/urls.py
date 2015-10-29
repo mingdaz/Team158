e11e9a6dfd34528c1c2a0297ca7ff1c09ccafeda
jingwen qiang <jxq48@jingwens-MBP.wv.cc.cmu.edu>
2015-10-29 13:15:49 -0400

    modify url

--- chingoal/store/urls.py
+++ chingoal/store/urls.py
@@ -2,5 +2,7 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
-   
+    url(r'^buy-title$', 'store.views.buy_title', name= 'buyTitle'),
+    url(r'^unlock-learning$', 'store.views.unlock_learning', name= 'unlockLearning'),
+    url(r'^earn-money$', 'store.views.earn_money', name= 'earnMoney'),
 ]
