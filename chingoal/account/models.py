466213d2b34830b5a9e87af0811cd10c2470d4af
jingwen qiang <jxq48@jingwens-MBP.wv.cc.cmu.edu>
2015-10-28 23:03:12 -0400

    Modify models.py

--- chingoal/account/models.py
+++ chingoal/account/models.py
@@ -5,25 +5,25 @@ from django.contrib.auth.models import User
 class Learner(models.Model):
     user = models.OneToOneField(User, unique = True, related_name="learner")
     TITLE_CHOICES = (('A', 'Primary School'), ('B', 'Middle School'), \
-                     ('C', 'High School'), ('D', 'College'))
-                     title = models.CharField(max_length = 20, default = TITLE_CHOICES[0][0], choices = TITLE_CHOICES)
-                     progress_level = models.IntegerField(default = 1)
-                     progress_lesson = models.IntegerField(default = 1)
-                     user_vm = models.IntegerField(default = 0)
-                     lesson_plan = models.IntegerField(default = 1)  # MAX = 5
-                     follows = models.ManyToManyField(User)
-                     bio = models.CharField(max_length = 420, default='Please introduce yourself')
-                     photo = models.ImageField(upload_to = 'user_photo', blank = True)
-                     
-                     def __unicode__(self):
-                         return 'Learner' + self.user.username
+        ('C', 'High School'), ('D', 'College'))  
+    title = models.CharField(max_length = 20, default = TITLE_CHOICES[0][0], choices = TITLE_CHOICES)
+    progress_level = models.IntegerField(default = 1)
+    progress_lesson = models.IntegerField(default = 1)
+    user_vm = models.IntegerField(default = 0)
+    lesson_plan = models.IntegerField(default = 1)  # MAX = 5
+    follows = models.ManyToManyField(User)
+    bio = models.CharField(max_length = 420, default='Please introduce yourself')
+    photo = models.ImageField(upload_to = 'user_photo', blank = True)
+
+    def __unicode__(self):
+        return 'Learner' + self.user.username
 
 
 class Teacher(models.Model):
     user = models.OneToOneField(User, unique = True, related_name="teacher")
     bio = models.CharField(max_length = 420, default='Please introduce yourself')
     photo = models.ImageField(upload_to = 'user_photo', blank = True)
-    
+
     def __unicode__(self):
         return 'Teacher' + self.user.username
 
