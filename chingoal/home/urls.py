6fcb0fd721001b9f07d602cb4e1bb6246e1a28cf
WangNing <wangning@WangNingdeMBP.wv.cc.cmu.edu>
2015-10-29 15:44:11 -0400

    Update home application

--- chingoal/home/urls.py
+++ chingoal/home/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^$', 'home.views.home'),
+    url(r'^edit-profile$', '../account.views.edit_profile', name = 'editProfile' ),
     url(r'^logout$', 'django.contrib.auth.views.logout_then_login', name = 'logout'),
 ]
