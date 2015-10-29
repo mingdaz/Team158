d0360085d6f41076f83ee189876a42fcb327a202
mingdaz <zhangmd406@gmail.com>
2015-10-29 17:04:56 -0400

    change login style

--- chingoal/account/forms.py
+++ chingoal/account/forms.py
@@ -1,6 +1,10 @@
 from django import forms
-
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
+
+class MyAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder':'Password'}))
 
 class RegistrationForm(forms.Form):
     username = forms.CharField(max_length = 30, widget = forms.TextInput(attrs={'class':'form-control','placeholder':'Username','autofocus':'true'}))
