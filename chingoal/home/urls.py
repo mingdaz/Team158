5bd4b5bc4eb5dc2e079e8323944858eb2e062906
jingwen qiang <jxq48@jingwens-MBP.wv.cc.cmu.edu>
2015-11-05 00:05:17 -0500

    Modify url

--- chingoal/home/urls.py
+++ chingoal/home/urls.py
@@ -1,9 +1,10 @@
-
+from django.contrib import admin
 from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^$', '../home.views.home',name = 'home'),
     url(r'^edit-profile$', '../account.views.edit_profile', name = 'editProfile' ),
     url(r'^edit-schedule$', '../account.views.edit_schedule', name= 'editSchedule'),
+    url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'login.html'}),
     url(r'^logout$', 'django.contrib.auth.views.logout_then_login', name = 'logout'),
 ]
