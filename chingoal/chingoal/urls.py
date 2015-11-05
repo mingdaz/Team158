42ef085911b1fe8ce8745a669470a640f1fbef30
jingwen qiang <jxq48@jingwens-MacBook-Pro.local>
2015-11-05 13:25:14 -0500

    Modify setting for photo path

--- chingoal/chingoal/urls.py
+++ chingoal/chingoal/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -26,4 +28,4 @@ urlpatterns = patterns('',
     url(r'^testpage/', include('testpage.urls')),
     # Ajax urls
     # transfer search parameters as get parameters
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
