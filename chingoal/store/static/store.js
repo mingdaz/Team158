5dae352c7c5f865aa6e123682f5c8fbfe78e258f
jingwen qiang <jxq48@jingwens-MacBook-Pro.local>
2015-11-06 01:36:16 -0500

    Create store.js

--- chingoal/store/static/store.js
+++ chingoal/store/static/store.js
@@ -1,31 +1,11 @@
-    var = LEVELS["One level","Two levels","Three levels","Four levels","Five levels"];
-    document.getElementById("myButton1").onclick = function () {
-        var r=confirm("Do you want to unlock lesson 1?");
-        if (r==true) {
-            location.href = "unlock-learning/LEVELS[0]";
-        }       
-    };
-    document.getElementById("myButton2").onclick = function () {
-        var r=confirm("Do you want to unlock lesson 2?");
-        if (r==true) {
-            location.href = "unlock-learning/LEVELS[1]";
-        }       
-    };
-    document.getElementById("myButton3").onclick = function () {
-        var r=confirm("Do you want to unlock lesson 3?");
-        if (r==true) {
-            location.href = "unlock-learning/LEVELS[2]";
-        }       
-    };
-    document.getElementById("myButton4").onclick = function () {
-        var r=confirm("Do you want to unlock lesson 4?");
-        if (r==true) {
-            location.href = "unlock-learning/LEVELS[3]";
-        }       
-    };
-    document.getElementById("myButton5").onclick = function () {
-        var r=confirm("Do you want to unlock lesson 5?");
-        if (r==true) {
-            location.href = "unlock-learning/LEVELS[4]";
-        }       
-    };
+    function unlock(id, unlock) {
+        var val = parseInt(document.getElementById(id).value,10);
+        if ((val - unlock) > 1) {
+            alert("Please unlock in order!");
+        } else {
+            var r = confirm("Do you want to unlock lesson "+val+"?");
+            if (r == true) {
+                location.href = "unlock-learning/"+val;
+            }
+        }
+    }
