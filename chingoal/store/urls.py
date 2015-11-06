ef58e20f1ee80c07707ac7e77331063bb880b2b7
mingdaz <zhangmd406@gmail.com>
2015-11-05 20:47:16 -0500

    update store view,url,homepage

--- chingoal/store/urls.py
+++ chingoal/store/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
+    url(r'^$', 'store.views.home', name= 'store'),
     url(r'^buy-title$', 'store.views.buy_title', name= 'buyTitle'),
     url(r'^unlock-learning$', 'store.views.unlock_learning', name= 'unlockLearning'),
     url(r'^earn-money$', 'store.views.earn_money', name= 'earnMoney'),
--- chingoal/store/views.py
+++ chingoal/store/views.py
@@ -5,14 +5,19 @@ from django.contrib.auth.decorators import login_required
 from models import *
 # Create your views here.
 @login_required
+def home(request):
+    context = {}
+    return render(request,'store/store.html',context)
+
+@login_required
 def buy_title(request):
     context = {}
-    return render(request,'store.html',context)
+    return render(request,'store/store.html',context)
 
 @login_required
 def unlock_learning(request):
     context = {}
-    return render(request,'store.html',context)
+    return render(request,'store/store.html',context)
 
 @login_required
 def earn_money(request):
