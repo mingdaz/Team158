8ad45e99d662b80e004fba4ab64404031b5569ed
jingwen qiang <jxq48@jingwens-MBP.wv.cc.cmu.edu>
2015-11-05 22:52:58 -0500

    Update store

--- chingoal/store/views.py
+++ chingoal/store/views.py
@@ -7,17 +7,31 @@ from models import *
 @login_required
 def home(request):
     context = {}
+    cur_user = request.user.learner_user
+    context['cur_user'] = cur_user
     return render(request,'store/store.html',context)
 
 @login_required
-def buy_title(request):
-    context = {}
-    return render(request,'store/store.html',context)
+def buy_title(request, title):
+    request.user.learner_user.title = title
+    request.user.learner_user.save()
+    return redirect('/')
 
 @login_required
-def unlock_learning(request):
-    context = {}
-    return render(request,'store/store.html',context)
+def unlock_learning(request, lesson):
+    if lesson == "One lesson":
+        unlock = 1
+    elif lesson == "Two lessons":
+        unlock = 2
+    elif lesson == "Three lessons":
+        unlock = 3
+    elif lesson == "Four lessons":
+        unlock = 4
+    elif lesson == "Five lessons":
+        unlock = 5
+    request.user.learner_user.unlock = unlock
+    request.user.learner_user.save()
+    return redirect('/')
 
 @login_required
 def earn_money(request):
