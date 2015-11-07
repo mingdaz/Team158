a0e40328510c973c70d51afd5f8db5d567d0d04d
mingdaz <zhangmd406@gmail.com>
2015-11-06 21:48:23 -0500

    store

--- chingoal/store/static/js/store.js
+++ chingoal/store/static/js/store.js
@@ -3,9 +3,28 @@
         if ((val - unlock) > 1) {
             alert("Please unlock in order!");
         } else {
-            var r = confirm("Do you want to unlock lesson "+val+"?");
+            var r = confirm("Cost $5 to unlock lesson "+val+"?");
             if (r == true) {
                 location.href = "unlock-learning/"+val;
             }
         }
+    }
+
+    function buyTitle(num, title) {
+        var titles = ["Pupil","Freshman","Sophomore","Junior","Senior","Graduate"];
+        if (title == titles[num]) {
+            alert("This is your current title!");
+        } else if (num == 0) {
+            var r = confirm("Free to buy title Pupil");
+            if (r == true) { 
+                location.href = "buy-title/"+titles[num]+"/"+0; 
+            }
+        } else {
+        var cost = 2 + num;
+        var r = confirm("Cost $"+cost+" to buy title "+titles[num]+"?");
+        if (r == true) { 
+            location.href = "buy-title/"+titles[num]+"/"+cost; 
+        }
+        }
+                    
     }
