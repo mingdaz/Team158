3af91d1ced924bcd80fc065cfcf05bf1342734fc
jgong1 <michaelgongjian@gmail.com>
2015-11-07 13:13:58 -0500

    add link to discussion board

--- chingoal/discussion/views.py
+++ chingoal/discussion/views.py
@@ -10,16 +10,21 @@ from django.http import HttpResponse, Http404
 @login_required
 # @transaction.atomic
 def post_question(request):
-    return render(request, 'discussion_board.html', {})
+    return render(request, 'discussion/discussion_board.html', {})
 
 
 @login_required
 # @transaction.atomic
 def reply_question(request):
-    return render(request, 'discussion_reply.html', {})
+    return render(request, 'discussion/discussion_reply.html', {})
 
 
 @login_required
 # @transaction.atomic
 def delete_post(request):
-    return render(request, 'discussion_board.html', {})
+    return render(request, 'discussion/discussion_board.html', {})
+
+
+@login_required
+def discussion_home(request):
+    return render(request, 'discussion/discussion_board.html', {})
