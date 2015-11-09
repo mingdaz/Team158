0776a86e1d856c46fd2025ee3fddcb64ba7a31fb
jingwen qiang <jxq48@jingwens-MacBook-Pro.local>
2015-11-09 12:33:12 -0500

    add recommend friends function

--- chingoal/account/views.py
+++ chingoal/account/views.py
@@ -183,16 +183,29 @@ def view_profile(request, uname):
 
     followers_learner = Learner.objects.none()
     followers_teacher = Teacher.objects.none()
+    unfollowers_learner = Learner.objects.none()
+    unfollowers_teacher = Teacher.objects.none()
+
+    unfollowers_learner = Learner.objects.exclude(user__in=follow_users)
+    if unfollowers_learner.filter(user__exact = cur_user):
+        unfollowers_learner = unfollowers_learner.exclude(user__exact = cur_user)
+
+    unfollowers_teacher = Teacher.objects.exclude(user__in=follow_users)
+    if unfollowers_teacher.filter(user__exact = cur_user):
+        unfollowers_teacher = unfollowers_teacher.exclude(user__exact = cur_user)
+
     if Learner.objects.filter(user__in=follow_users):
         followers_learner = Learner.objects.filter(user__in=follow_users)
+
 
     if Teacher.objects.filter(user__in=follow_users):
         followers_teacher = Teacher.objects.filter(user__in=follow_users)
 
     followers = list(chain(followers_learner,followers_teacher))
-    print followers
+    unfollowers = list(chain(unfollowers_learner,unfollowers_teacher))
+
     context['followers'] = followers
-    print Learner.objects.all()
+    context['unfollowers'] = unfollowers
 
 #    if Learner.objects.filter(user__exact = request.user):
 #        context['isLearner'] = 'yes'
@@ -304,7 +317,7 @@ def edit_schedule(request):
 @login_required
 def follow(request, uname, isFollowing, isLearner):
     followee = User.objects.get(username__exact = uname)
-    if isLearner == 'yes':
+    if Learner.objects.filter(user__exact = request.user):
         follower = request.user.learner_user
     else:
         follower = request.user.teacher
