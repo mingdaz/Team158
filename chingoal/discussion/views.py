6a2f4e61ede460df2bf01897a2ca96b0c0e4eda6
jgong1 <michaelgongjian@gmail.com>
2015-11-09 02:13:27 -0500

    add views method

--- chingoal/discussion/views.py
+++ chingoal/discussion/views.py
@@ -11,6 +11,8 @@ from django.db import transaction
 from forms import *
 from models import *
 
+from account.models import *
+
 
 @login_required
 # @transaction.atomic
@@ -34,9 +36,11 @@ def delete_post(request):
 def discussion_home(request):
     posts = Post.objects.all().order_by('-post_time')
     post_replies = [];
+    i = len(posts)
     for post in posts:
         number_replies = len(Reply.objects.filter(reply_to = post))
-        post_replies.append({'post':post, 'number_replies' : number_replies})
+        post_replies.append({'post':post, 'number_replies' : number_replies, 'list_id' : i})
+        i -= 1
     context = {'username' : request.user.username, 'posts' : post_replies}
     return render(request, 'discussion/discussion_board.html', context)
 
@@ -47,19 +51,29 @@ def discussion_reply(request, post_id):
     post_user = post.author
     replies = Reply.objects.filter(reply_to__id = post_id)
     max_reply_id = replies.aggregate(Max('id'))['id__max'] or 0
+
+    user_temp = Learner.objects.filter(user = request.user)
+    is_learner = len(user_temp)
+    if is_learner == 1:
+        cur_user = user_temp[0]
+    else:
+        cur_user = Teacher.objects.get(user = request.user)
+
     return render(request, 'discussion/discussion_reply.html', \
         {'post': post, 'replies' : replies, 'post_user': post_user, 'max_reply_id' : max_reply_id,\
-            'username' : request.user.username})
+            'username' : request.user.username, 'cur_user' : cur_user})
 
 
 @login_required
 def get_posts(request):
     all_posts = Post.objects.all().order_by('-post_time')
     post_replies = []
+    i = len(all_posts)
     for post in all_posts:
         replies = Reply.objects.filter(reply_to__id = post.id).order_by('post_time')
-        context_temp = {'post' : post, 'replies' : replies}
+        context_temp = {'post' : post, 'replies' : replies, 'list_id' : i, 'number_replies' : len(replies)}
         post_replies.append(context_temp)
+        i -= 1
     context = {'post_replies' : post_replies}
     return render(request, 'discussion/posts.json', context, content_type = 'application/json')
 
@@ -121,7 +135,6 @@ def delete_post(request, post_id):
 @login_required
 @transaction.atomic
 def delete_reply(request, reply_id):
-    print 'in delete reply'
     replyTemp = Reply.objects.get(id = reply_id)
     post_id = replyTemp.reply_to.id
     replyTemp.delete()
